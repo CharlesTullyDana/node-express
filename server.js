2c4d6db9d8c516b189c6d217ef457743af03060d
Tully Dana <tullydana@Tullys-iMac.local>
2024-05-15 11:04:00 -0700

    part 2 Express

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.json());
 
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+})
 app.all('/campsites', (req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
